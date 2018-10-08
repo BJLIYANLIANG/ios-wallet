f7c4cbf10b665f95744ccd3d8a133233b905a471
Vladimir Benkevich <vladimir.benkevich@devpool.com>
2018-10-08 17:51:38 +0300

    project structure

---
+++ src/EthereumWallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ethereum Wallet
+//
+//  Created by Vladimir Benkevich on 08/10/2018.
+//  Copyright © 2018 devpool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
