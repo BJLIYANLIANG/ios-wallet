2c510394b0752921b1ce64dc0e0063ba80329e0f
Vladimir Benkevich <vladimir.benkevich@devpool.com>
2018-10-26 19:29:16 +0300

    fix: app crashed if amount of wei > int64.max

--- src/EthereumWallet/Model/Wei.swift
+++ src/EthereumWallet/Model/Wei.swift
@@ -23,15 +23,27 @@ extension IntHex {
             return Int64(self, radix: 10)
         }
     }
+
+    var decimal: Decimal? {
+        if self.starts(with: "0x") {
+            return decimalFrom(bigHex: String(self.dropFirst(2)))
+        } else {
+            return Decimal(string: self)
+        }
+    }
 }
 
 extension Wei {
 
     var ether: Ether? {
-        if let int = intValue {
-            return Ether(sign: .plus, exponent: -weiExponent, significand: Decimal(int))
-        } else {
+        guard let decimal = self.decimal else {
             return nil
+        }
+
+        if self.starts(with: "0x") {
+            return Ether(sign: .plus, exponent: -weiExponent, significand: decimal)
+        } else {
+            return Ether(sign: .plus, exponent: -weiExponent, significand: decimal)
         }
     }
 }
@@ -49,4 +61,37 @@ extension Int64 {
     var hex: IntHex {
         return "0x" + String(self, radix: 16)
     }
+}
+
+extension String {
+
+    subscript(_ range: CountableRange<Int>) -> String {
+        let idx1 = index(startIndex, offsetBy: max(0, range.lowerBound))
+        let idx2 = index(startIndex, offsetBy: min(self.count, range.upperBound))
+        return String(self[idx1..<idx2])
+    }
+}
+
+func decimalFrom(bigHex value: String, groupLenght: Int = 12) -> Decimal? {
+    let groups = 1 + value.count / groupLenght
+    let exp = pow(16, groupLenght)
+
+    var result: Decimal = 0
+
+    for i in 1...groups {
+        let offset = value.count - i * groupLenght
+        let from = max(offset, 0)
+        let count = min(groupLenght + offset, groupLenght)
+        let part = value[from..<from + count]
+
+        guard let int = Int64(part, radix: 16) else {
+            return nil
+        }
+
+        let decimal = Decimal(int)
+
+        result += decimal * pow(exp, i)
+    }
+
+    return result
 }
